621ec06ba96196b3905afd1bcaa71c2f4d68f097
Saurabh Singh <sighsaurabh920@gmail.com>
2020-04-04 18:48:03 +0000

    Initial commit

---
+++ src/main/java/org/worldbuild/project/WbAwsSdkProjectApplication.java
@@ -0,0 +1,18 @@
+package org.worldbuild.project;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.worldbuild.project.utils.ScreteManagerUtils;
+
+@Log4j2
+@SpringBootApplication
+public class WbAwsSdkProjectApplication {
+
+	public static void main(String[] args) {
+		ScreteManagerUtils.getSecretV1();
+		//ScreteManagerUtils.getSecretV2();
+		SpringApplication.run(WbAwsSdkProjectApplication.class, args);
+	}
+
+}
