92f5a42b5dfc870984e368514e22e1147300f0dc
Saurabh Singh <singhsaurabh920@gmail.com>
2020-08-18 04:23:45 +0000

    modal attributes in thymeleaf

--- src/main/java/org/worldbuild/aws/constant/PageViewDetail.java
+++ src/main/java/org/worldbuild/aws/constant/PageViewDetail.java
@@ -2,9 +2,9 @@ package org.worldbuild.aws.constant;
 
 public enum PageViewDetail {
 
-    DASHBOARD_V1("index1","Dashboard V1"),
-    DASHBOARD_V2("index2","Dashboard V2"),
-    DASHBOARD_V3("index3","Dashboard V3"),
+    DASHBOARD_V1("dashboard/index1","Dashboard V1"),
+    DASHBOARD_V2("dashboard/index2","Dashboard V2"),
+    DASHBOARD_V3("dashboard/index3","Dashboard V3"),
     S3_SERVICE("s3","S3 Service");
 
     public String path;
